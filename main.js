17d81626841677c9a4dd4ecffd99a796d152d380
Eros | Jotalevi <talevineto@gmail.com>
2024-10-07 20:05:16 -0300

    Added dark mode

--- main.js
+++ main.js
@@ -15,3 +15,20 @@ document.addEventListener('DOMContentLoaded',
         });
     }
 )
+
+document.isDarkMode = true;
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+  document.querySelector('body').classList.add('dark light');
+  document.querySelector('body').classList.remove(document.isDarkMode ? 'light' : 'dark');
+});
+
+toggleDarkMode = () => {
+  localStorage.setItem('darkMode', !document.isDarkMode);
+  document.isDarkMode = !document.isDarkMode;
+
+  document.querySelector('body').classList.remove(document.isDarkMode ? 'light' : 'dark');
+  document.querySelector('body').classList.add(document.isDarkMode ? 'dark' : 'light');
+}
