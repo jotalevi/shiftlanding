68d5a887f8e80db16e3099bab8f0cfdb56315fdc
unknown <cetecom@AVAWSV03LC1104.duoc.acad>
2024-10-07 19:59:10 -0300

    Added XHR requests to lambda for contact handles

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ toggleFAQShow = (element) => {
 }
 
 document.addEventListener('DOMContentLoaded',
-    function(){
-        document.querySelectorAll('.loading').forEach((element) => {
-            element.classList.remove('loading');
-        });
-    }
+  function () {
+    document.querySelectorAll('.loading').forEach((element) => {
+      element.classList.remove('loading');
+    });
+  }
 )
 
 toggleDarkMode = () => {
@@ -21,31 +21,57 @@ toggleDarkMode = () => {
   document.isDarkMode = !document.isDarkMode;
 
   document.querySelector('body').classList.remove(document.isDarkMode ? 'light' : 'dark');
-  document.querySelector('body').classList.add(document.isDarkMode ? 'dark' : 'light');
+  document.querySelector('body').classList.add(document.isDarkMode ? 'dark' : 'light');   
 }
 
 sendContactMail = () => {
-  let opts = {
+  var data = JSON.stringify({
     firstName: document.getElementById('firstNameInputContact').value,
     lastName: document.getElementById('lastNameInputContact').value,
     email: document.getElementById('businessMailInputContact').value,
     companyName: document.getElementById('companyNameInputContact').value,
     jobTitle: document.getElementById('jobTitleInputContact').value,
     message: document.getElementById('messageInputContact').value
-  };
+  });
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("POST", "https://g48yfip4aa.execute-api.us-east-2.amazonaws.com/default/contact_handle");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(data);
 
   console.log(opts);
 }
 
 sendScheduleCall = () => {
-  let opts = {
+  var data = JSON.stringify({
     firstName: document.getElementById('firstNameInputCall').value,
     lastName: document.getElementById('lastNameInputCall').value,
     email: document.getElementById('businessMailInputCall').value,
     phone: document.getElementById('phoneNumberInputCall').value,
     companyName: document.getElementById('companyNameInputCall').value,
     jobTitle: document.getElementById('jobTitleInputCall').value,
-  };
+  });
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("POST", "https://g48yfip4aa.execute-api.us-east-2.amazonaws.com/default/contact_handle");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(data);
 
   console.log(opts);
 }
