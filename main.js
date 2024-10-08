d965c7c3a312d3fa8c0276325826e4b021b208bb
Eros | Jotalevi <talevineto@gmail.com>
2024-10-07 21:34:24 -0300

    Added formulary handles

--- main.js
+++ main.js
@@ -23,3 +23,29 @@ toggleDarkMode = () => {
   document.querySelector('body').classList.remove(document.isDarkMode ? 'light' : 'dark');
   document.querySelector('body').classList.add(document.isDarkMode ? 'dark' : 'light');
 }
+
+sendContactMail = () => {
+  let opts = {
+    firstName: document.getElementById('firstNameInputContact').value,
+    lastName: document.getElementById('lastNameInputContact').value,
+    email: document.getElementById('businessMailInputContact').value,
+    companyName: document.getElementById('companyNameInputContact').value,
+    jobTitle: document.getElementById('jobTitleInputContact').value,
+    message: document.getElementById('messageInputContact').value
+  };
+
+  console.log(opts);
+}
+
+sendScheduleCall = () => {
+  let opts = {
+    firstName: document.getElementById('firstNameInputCall').value,
+    lastName: document.getElementById('lastNameInputCall').value,
+    email: document.getElementById('businessMailInputCall').value,
+    phone: document.getElementById('phoneNumberInputCall').value,
+    companyName: document.getElementById('companyNameInputCall').value,
+    jobTitle: document.getElementById('jobTitleInputCall').value,
+  };
+
+  console.log(opts);
+}
